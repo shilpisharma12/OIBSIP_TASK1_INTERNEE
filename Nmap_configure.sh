51522ea70719826b843970a9f9486bfeb20b1336
shilpi sharma <limitlessdesign.in@gmail.com>
2025-07-15 22:26:56 +0530

    Create Nmap_configure.sh

---
+++ Nmap_configure.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#network scanning with nmap/zenmap
+
+echo"________________"
+
+echo "target: to configure and gather information form the ip addresses or domain name which we provided."
+
+echo" run cmd: install namp"
+
+echo" run cmd: nmap domain name or ip address."
+
+echo" configure the ports and services as well as state of the ports."
+
+echo"_______________"
